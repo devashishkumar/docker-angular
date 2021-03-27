13c6b0c365cbb1bd7adca535aeb51cff0c7e1952
= <=>
2021-03-27 13:10:31 +0530

    update port in server js

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/docker-angular-app'));
-window.console.log('8');
+// window.console.log('8');
 app.get('/*', function (req, res) {
-    window.console.log('10');
+    // window.console.log('10');
     res.sendFile(path.join(__dirname + '/dist/docker-angular-app/index.html'));
-    console.log(req, res, '12');
+    // console.log(req, res, '12');
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8888);
